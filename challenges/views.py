b2287b97d2e67a41a828ba7c1993e53f8506e90c
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-08 21:26:10 +0300

    Tags and template inheritance

--- challenges/views.py
+++ challenges/views.py
@@ -14,20 +14,24 @@ monthly_challenges = {
     "september": "Learn Django for at least 20 minutes every day!",
     "october": "Eat no meat for the entire month!",
     "november": "Walk for at least 20 minutes every day!",
-    "december": "Learn Django for at least 20 minutes every day!"
+    "december": None
 }
 
 
 def index(request):
-    list_items = ""
+    # list_items = ""
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
 
 
 def monthly_challenge_by_number(request, month):
