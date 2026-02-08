bf3eac8858fd4139ae2d93c7c5e6385c04e20064
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-08 23:54:29 +0300

    Adding static files and global static files. Css styling.

--- mypage/settings.py
+++ mypage/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # BASE_DIR / "challenges" / "templates"
-            BASE_DIR /"templates"
+            BASE_DIR / "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
